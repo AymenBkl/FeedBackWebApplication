e55a8230e3e5c9f271ca22db7e0b6fa5ba98a22a
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2020-06-28 14:06:35 +0100

    get,post working fine

--- Server-Node-Js/form-submission/routes/users.js
+++ Server-Node-Js/form-submission/routes/users.js
@@ -2,9 +2,9 @@ var express = require('express');
 const mongoose = require('mongoose');
 var router = express.Router();
 var feedbackmodel = require('../models/feedback');
-const url = "mongodb://localhost:27017/aymenxyz";
+const url = "mongodb://localhost/aymenxyz";
 
-mongoose.connect(url);
+mongoose.connect(url,{ useNewUrlParser: true });
 
 
 mongoose.connection.on("error", err => {
@@ -17,10 +17,21 @@ mongoose.connection.on("connected", (err, res) => {
 var user = {'name ' : "aymen",'lastname' : "bkl"}
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json(user);
+  feedbackmodel.find({}).then(function(data){
+    res.json(data);
+  }).catch(function(err){
+    console.log("errrrrrr",err);
+  })
 });
 
 router.post('/post',function(req,res,next) {
-  console.log(req.body);
+  var feedback = new feedbackmodel(req.body);
+  feedback.save()
+          .then(function(feedback) {
+            res.json(feedback);
+          })
+          .catch(function(err){
+            res.send(err);
+          });
 });
 module.exports = router;
