cafe563c88d829b2d2540b49d7272395f3dbe2d9
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2020-06-28 13:12:41 +0100

    adding mongoose

--- Server-Node-Js/form-submission/routes/users.js
+++ Server-Node-Js/form-submission/routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+var user = {'name ' : "aymen",'lastname' : "bkl"}
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json(user);
 });
 
+router.post('/post',function(req,res,next) {
+  console.log(req.body);
+});
 module.exports = router;
